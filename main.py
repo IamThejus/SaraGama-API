836b03304d82e343834a04a7701d5c5dd7d77dc5
Thejus Asokan <92664795+IamThejus@users.noreply.github.com>
2026-02-06 00:55:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from yt_downloader import *
 from models import *
 from superdb import *
 from upload_song_cloudinary import *
+import os
+import uvicorn
 
 app=FastAPI()
 
@@ -32,3 +34,8 @@ def home():
 def add_song_to_library(url:str,background_task:BackgroundTasks):
     background_task.add_task(add_song,url)
     return True
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
